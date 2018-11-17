d23766704bd2eacbd821c6802b9091893116cd19
alexOderdenge <alexoderdengexela@hotmail.com>
2018-11-17 12:07:18 -0200

    Início
    Criando o repositório.

---
+++ src/main/java/TrabalhoDsw/TrabalhoDsw/TrabalhoDswApplication.java
@@ -0,0 +1,12 @@
+package TrabalhoDsw.TrabalhoDsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDswApplication.class, args);
+	}
+}
